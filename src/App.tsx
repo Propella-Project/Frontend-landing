d54cf235fc1893eb1317951ce6bb753aa81e4578
Rosvic <rosemaryoguezuonu@gmail.com>
2026-03-04 15:41:24 +0100

    Ready for deployment with fixed error 405

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import {
   Routes,
   Route,
   useSearchParams,
-  // useNavigate,
 } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
